a7491605e27fd1494e4ca7152290a15dfc3b1637
wirthit <simon@redamber.com>
2020-07-25 09:34:53 +0100

    Lookup

---
+++ HesaUser.Lookup/Models/Users/Users.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HesaUser.Lookup.Models.Users
+{
+    public class Users
+    {
+    }
+}
